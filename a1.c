9bce528f493de79517df6ed09b04f312c4475d87
fakiru12 <miz.rares@yahoo.com>
2024-04-08 16:31:54 +0300

    reading multiple files works

--- a1.c
+++ a1.c
@@ -6,7 +6,7 @@
 #include <sys/types.h>
 #include <dirent.h>
 
-void parseDir(char *path){
+void parseDir(char *path, int x){
     DIR *dir;
     struct dirent *entry;
     if(!(dir = opendir(path))){
@@ -14,25 +14,32 @@ void parseDir(char *path){
     }
     else{
         while((entry = readdir(dir)) != NULL){
-            if(entry->d_type == DT_DIR)
-                if(strcmp(entry->d_name,".") == 0 || strcmp(entry->d_name,"..") == 0)
-                       printf("%s %s\n",path, entry->d_name);
-				else{
-					char path2[1024];
-					strcat(path2, path);
-					strcat(path2,"/");
-					strcat(path2, entry->d_name);
-					printf("%s\n",path2);				
-				}
+            if(strcmp(entry->d_name,".") != 0 && strcmp(entry->d_name,"..") != 0){
+                for(int i = 0; i < x; i++)
+                    printf("| ");
+                printf("|_");
+                printf("%s\n", entry->d_name);
+                if(entry->d_type == DT_DIR){
+                    char path2[1024] = "";
+                    strcat(path2,path);
+                    strcat(path2,"/");
+                    strcat(path2,entry->d_name);
+                    parseDir(path2,x+1);   
+                }
+            }
         }
     }
     closedir(dir);
 }
 
 int main(int argc, char* argv[]){
-    if(argc == 2){
-        parseDir(argv[1]);
+    if(argc >= 3){
+        int x = atoi(argv[1]);
+        for(int i = 1; i <= x; i++){
+            printf("%s\n",argv[1 + i]);
+            parseDir(argv[1 + i],0);
+            printf("\n");
+        }
     }
     return 0;
-    //this is a comment
 }
